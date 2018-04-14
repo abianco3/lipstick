7b1a04e0dc4731dce63a412e81c6d47d04c2c89a
Anthony Bianco <anthony.bianco3@gmail.com>
2018-04-14 10:23:45 -0700

    [FEAT] Use work sans as default font

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/react';
-
-const r = require.context('../app', true, /\*.story\.js$/);
-
-function loadStories () {
-  r.keys().forEach((r));
-}
-
-configure(loadStories, module);
